0154ec86c52b51cce451ec109af0f5e5cffcc3ee
fauzan.habiburrohman <fauzan.habiburrohman@smartfren.com>
2023-02-17 07:10:58 +0700

    update port

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"awesomeProject2/app"
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/tylerb/graceful"
 	"net/http"
@@ -13,10 +14,11 @@ func main() {
 	srv := &graceful.Server{
 		Timeout: 10 * time.Second,
 		Server: &http.Server{
-			Addr:    "8888",
+			Addr:    ":8888",
 			Handler: e,
 		},
 	}
 	app.AssignRouting(e)
 	srv.ListenAndServe()
+	fmt.Println("servcer Run")
 }
